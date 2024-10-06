638c13e6fce30d82dd1eb76d695c3191f6bdb645
Kateřina Nazarčuková <katka.nazarcukova@gmail.com>
2024-10-06 20:36:48 +0200

    WIP: structure motif search automation
    - function for definition of residues
      - implemented chain renaming and correct label_seq_id numbering
    sequence

--- src/structure_motif_search.py
+++ src/structure_motif_search.py
@@ -1,48 +1,116 @@
 from argparse import ArgumentParser
 import json
 from pathlib import Path
+from Bio.PDB.Chain import Chain
+from Bio.PDB.Residue import Residue
+from rcsbsearchapi.search import StructMotifQuery, StructureMotifResidue
 import shutil
 
-from config import Config
+from Bio.PDB.PDBParser import PDBParser
+
+# from config import Config
 
 
-def extract_representatives(sugar: str, align_method: str, representatives_file: str, config: Config, input_folder: Path) -> None:
-    """
-    Extract files for structure motif search
+# def extract_representatives(sugar: str, align_method: str, num: int, method: str, config: Config, input_folder: Path) -> None:
+#     """
+#     Extract files for structure motif search
+#
+#     :param sugar: The sugar for which representative binding sites are being defined
+#     :param align_method: The PyMOL command that was used for alignment
+#     :param representatives_file: The file containing the representative binding sites ids
+#     :param config: Config object
+#     :param input_folder: Folder to extract representatives in
+#     """
+#
+#     path_to_file: Path = config.binding_sites / f"{sugar}_fixed_5"
+#
+#     with open(config.results_folder / "clusters" / sugar / align_method / f"{num}_{method}_cluster_representatives.json") as rep_file:
+#         representatives: dict = json.load(rep_file)
+#     with open(config.results_folder / "clusters" / sugar / f"{sugar}_structures_keys.json") as struct_keys_file:
+#         structure_keys: dicutil.copyfile((path_to_file / structure), (input_folder / structure))
 
-    :param sugar: The sugar for which representative binding sites are being defined
-    :param align_method: The PyMOL command that was used for alignment
-    :param representatives_file: The file containing the representative binding sites ids
-    :param config: Config object
-    :param input_folder: Folder to extract representatives in
-    """
 
-    path_to_file: Path = config.binding_sites / f"{sugar}_fixed_5"
+# def load_representatives(config: Config) -> list:
+    # representatives = []
+    # for file in sorted(Path("../results/structure_motif_search/input_representatives/FUC/").glob("*.pdb")):
+        # representatives.append(file)
 
-    with open(config.results_folder / "clusters" / sugar / align_method / representatives_file) as rep_file:
-        representatives: dict = json.load(rep_file)
-    with open(config.results_folder / "clusters" / sugar / f"{sugar}_structures_keys.json") as struct_keys_file:
-        structure_keys: dict = json.load(struct_keys_file)
+    # return representatives
 
-    for _, file_key in representatives.items():
-        structure = structure_keys[str(file_key)]
-        shutil.copyfile((path_to_file / structure), (input_folder / structure))
 
+def get_struc_name(path_to_file: Path) -> str:
+    return (path_to_file.name).split("_")[1]
+
+
+def define_residues(path_to_file: Path, struc_name: str) -> list:
+    parser = PDBParser()
+
+    structure = parser.get_structure(struc_name, path_to_file)
+
+    #TODO: needed parametrs: rmsd_cutoff 3, return_type assembly, attribute: "rcsb_comp_model_provenance.source_db", "operator": "exact_match", "negation": false, "value": "AlphaFoldDB
+
+    models = list(structure)
+    if len(models) < 1:
+        raise ValueError("More than one model in the structure!")
+
+    chains = list(models[0])
+
+    sorted_chains = sorted(chains, key = lambda c: c.get_id())
+    chain_id_map = {}
+    key = "A"
+    for ch in sorted_chains:
+        chain_id_map[ch.get_id()] = key
+        key = chr(ord(key) + 1)
+
+    residues = []
+    for chain in chains:
+        i = 1
+        for residue in chain: #NOTE: also possible to only iterate over all residues in a model
+            residue: Residue = residue
+            chain: Chain = chain
+            # Excludes fucose
+            if residue.get_id()[0] == " ":
+                residues.append(StructureMotifResidue(struct_oper_id="1", chain_id=chain_id_map[chain.get_id()], label_seq_id=i)) # type: ignore
+                i += 1
+
+    # print(residues)
+    # print(len(residues))
+
+    if len(residues) > 10:
+        raise ValueError(f"More than 10 residues in the binding site: {path_to_file.name}")
+
+    return residues
+
+
+def run_query(path_to_file: Path, residues: list):
+    query = StructMotifQuery(structure_search_type="file_upload", file_path=str(path_to_file), file_extension="pdb", residue_ids=residues, rmsd_cutoff=3, atom_pairing_scheme="ALL")
+    print(query.to_json())
+
+    print(list(query(return_type="assembly")))
+
+
+def struct_motif_search():
+    path_to_file = Path("../results/structure_motif_search/input_representatives/FUC/48_2fir_FUC_901_L.pdb")
+    struc_name = get_struc_name(path_to_file)
+    run_query(path_to_file, define_residues(path_to_file, struc_name))
+
+
+#TODO: check if original structure is returned
 
 if __name__ == "__main__":
-    parser = ArgumentParser()
-    
-    parser.add_argument("-f", "--file", help="File containing the keys of the representative binding sites", type=str, required=True)
-    parser.add_argument("-s", "--sugar", help="Three letter code of sugar", type=str, required=True)
-    parser.add_argument("-a", "--align_method", help="PyMOL cmd for the calculation of RMSD", type=str,
-                        choices=["super", "align"], required=True)
+    # parser = ArgumentParser()
+    #
+    # parser.add_argument("-s", "--sugar", help="Three letter code of sugar", type=str, required=True)
+    # parser.add_argument("-a", "--align_method", help="PyMOL cmd for the calculation of RMSD", type=str,
+    #                     choices=["super", "align"], required=True)
+    # parser.add_argument("-n", "--number", help="Number of clusters", type=str, required=True)
+    # parser.add_argument("-m", "--method", help="Cluster method", type=str, required=True)
+    #
+    # # args = parser.parse_args()
+    #
+    # config = Config.load("config.json")
+    # input_folder = (config.results_folder / "structure_motif_search" / "input_representatives" / args.sugar)
+    # input_folder.mkdir(exist_ok=True, parents=True)
 
-    args = parser.parse_args()
-
-    config = Config.load("config.json")
-    input_folder = (config.results_folder / "structure_motif_search" / "input_representatives" / args.sugar)
-    input_folder.mkdir(exist_ok=True, parents=True)
-
-    extract_representatives(args.sugar, args.align_method, args.file, config, input_folder)
-
-#TODO: implement advanced search api
+    # extract_representatives(args.sugar, args.align_method, args.number, args.method, config, input_folder)
+    struct_motif_search()
