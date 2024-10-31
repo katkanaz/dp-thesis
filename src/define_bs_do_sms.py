f2d8f5f307010f2594248b2dfdf80119a06f3536
Kateřina Nazarčuková <katka.nazarcukova@gmail.com>
2024-10-31 22:18:51 +0100

    feat: call module mains in main scripts

--- src/define_bs_do_sms.py
+++ src/define_bs_do_sms.py
@@ -4,25 +4,55 @@ Description: Define representative binding sites for a given sugar using PQ and 
 cluster obtained data and choose representatives to perform structure motif search with.
 Author: Kateřina Nazarčuková
 """
-#TODO: import libs
+
+#NOTE: what if a want to run the scripts one by one, just commnet it?
+#TODO: do i need all the libs from other scripts imported or just the ones used in this specidic script
 from argparse import ArgumentParser
 from pathlib import Path
+from platform import system
+from typing import Union
 
 from config import Config
 
-from run_pq import run_pq
+from run_pq import run_pq #TODO: should the main functions have documentation, in this way it wont be readable what it does
 from perform_alignment import perform_alignment
 from cluster_data import cluster_data
 from compare_clusters import compare_clusters
 from create_tanglegram import create_tanglegram
+from structure_motif_search import structure_motif_search
 
-def main():
+#TODO: is union not in python 3.8
+def main(sugar: str, align_method: str, n_clusters: int, cluster_method: str, make_dendrogram: bool,
+         color_threshold: Union[float, None] = None, config: Config, is_unix: bool):
+    run_pq(sugar, config, is_unix)
+    perform_alignment(sugar, align_method, config)
+    cluster_data(sugar, n_clusters, cluster_method, align_method, config, make_dendrogram, color_threshold)
+    compare_clusters()
+    create_tanglegram()
+    structure_motif_search()
 
 
 if __name__ == "__main__":
-    config = Config.load("config.json")
+    parser = ArgumentParser()
 
-    main()
+    parser.add_argument("-s", "--sugar", help="The sugar abbreviation", type="str", required=True)
+    parser.add_argument("-a", "--align_method", help="PyMOL cmd for the calculation of RMSD", choices=["super", "align"],
+                        type="str", required=True)
+    parser.add_argument("-n", "--n_clusters", help="Number of clusters to create", type=int)
+    parser.add_argument("-c", "--cluster_method", help="Clustering method", type=str,
+                        choices=["ward", "average", "centroid", "single", "complete", "weighted", "median"],
+                        required=True)
+    parser.add_argument("-d", "--make_dendrogram", action="store_true", help="Whether to create and save the dendrogram")
+    parser.add_argument("-t", "--color_threshold", type=float, help="Color threshold for dendrogram (default: None)")
+
+    args = parser.parse_args()
+
+    config = Config.load("config.json")#TODO: fix confing for python 3.8
+    is_unix = system() != "Windows"
+    #TODO: are positional?
+
+    #TODO: make the number of clusters optional
+    main(args.sugar, args.align_method, args.n_clusters, args.cluster_method, args.make_dendrogram, args.color_threshold, config, is_unix)
 
 
 
--- src/download_and_categorization.py
+++ src/download_and_categorization.py
@@ -4,9 +4,10 @@ Description: Download structures with sugars from PDB and perform sugar categori
 Author: Kateřina Nazarčuková
 """
 
-#TODO: import libs
+
 from argparse import ArgumentParser
 from pathlib import Path
+from platform import system
 
 from config import Config
 
@@ -18,11 +19,28 @@ from plot_graphs import plot_graphs
 from graph_analysis import graph_analysis
 from process_and_filter_ligands import process_and_filter_ligands
 
-def main():
+def main(config: Config, is_unix: bool, min_rscc: float, max_rscc: float, min_rmsd: float, max_rmsd: float):#TODO: is main appropriate name?
 
-    # run_mv()
+    # download_files(config)
+    categorize(config)
+    extract_rscc_and_resolution(config)
+    run_mv(config, is_unix)
+    plot_graphs(config)
+    graph_analysis(config, min_rscc, max_rscc, min_rmsd, max_rmsd)
+    process_and_filter_ligands(config)
 
 if __name__ == "__main__":
-    config = Config.load("config.json")
+    parser = ArgumentParser()
 
-    main()
+    parser.add_argument("--min_rscc", help="Minimum RSCC used to define graph area", type="float", required=True, default=0.85)
+    parser.add_argument("--max_rscc", help="Maximum RSCC used to define graph area", type="float", required=True, default=1.0)
+    parser.add_argument("--min_rmsd", help="Minimum RMSD used to define graph area", type="float", required=True, default=2.0)
+    parser.add_argument("--max_rmsd", help="Maximum RMSD used to define graph area", type="float", required=True, default=3.0)
+    #FIXME: where do the values come from
+
+    args = parser.parse_args()
+
+    config = Config.load("config.json")
+    is_unix = system() != "Windows"
+
+    main(config, is_unix, args.min_rscc, args.max_rscc, args.min_rmsd, args.max_rmsd)
