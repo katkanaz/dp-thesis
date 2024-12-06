6307a1ed9075a283f6cac5c48846a4154db17465
Kateřina Nazarčuková <katka.nazarcukova@gmail.com>
2024-12-06 19:43:52 +0100

    feat: new user config object added to deal with configuration given by the user

--- src/configuration.py
+++ src/configuration.py
@@ -5,11 +5,10 @@ from typing import List, Union
 from datetime import datetime
 import os
 
-from rcsbsearchapi.search import Optional
 from logger import logger
 
 
-class Config(BaseModel):
+class UserConfig(BaseModel):
     data_dir: Path
     results_dir: Path
     images_dir: Path
@@ -19,34 +18,45 @@ class Config(BaseModel):
     pdb_ids_list: List[str] # TODO: Handle if the list is not given in config.json
 
 
-    log_path: Optional[Path] = None
-    sugar_binding_patterns_dir: Optional[Path] = None
-    components_dir: Optional[Path] = None
-    mmcif_files_dir: Optional[Path] = None
-    no_o6_mmcif_dir: Optional[Path] = None
-    validation_files_dir: Optional[Path] = None
-    sugars_dir: Optional[Path] = None
-    categorization_dir: Optional[Path] = None
-    validation_dir: Optional[Path] = None
-    mv_run_dir: Optional[Path] = None
-    graph_analysis_dir: Optional[Path] = None
-    residue_graphs_dir: Optional[Path] = None
-    pq_run_dir: Optional[Path] = None
-    raw_binding_sites_dir: Optional[Path] = None
-    filtered_binding_sites_dir: Optional[Path] = None
-    clusters_dir: Optional[Path] = None
-    structure_motif_search_dir: Optional[Path] = None
-    dendrograms_dir: Optional[Path] = None
-    tanglegrams_dir: Optional[Path] = None
+    @classmethod
+    def load_json(cls, file_path: Union[Path, str]) -> "UserConfig":
+        with open(file_path, "r") as f:
+            data = json.load(f)
+        user_config = cls(**data)
+
+        return user_config
+
+
+class Config():
+    user_cfg: UserConfig
+
+    log_path: Path
+    sugar_binding_patterns_dir: Path
+    components_dir: Path
+    mmcif_files_dir: Path
+    no_o6_mmcif_dir: Path
+    validation_files_dir: Path
+    sugars_dir: Path
+    categorization_dir: Path
+    validation_dir: Path
+    mv_run_dir: Path
+    graph_analysis_dir: Path
+    residue_graphs_dir: Path
+    pq_run_dir: Path
+    raw_binding_sites_dir: Path
+    filtered_binding_sites_dir: Path
+    clusters_dir: Path
+    structure_motif_search_dir: Path
+    dendrograms_dir: Path
+    tanglegrams_dir: Path
 
 
     @classmethod
     def load(cls, file_path: Union[Path, str], sugar: Union[str, None], need_data_run: bool, force_new: bool = False) -> "Config":
-        with open(file_path, "r") as f:
-            data = json.load(f)
-        config = cls(**data)
+        config = cls()
+        config.user_cfg = UserConfig.load_json(file_path)
         current_run = config.get_current_run(force_new)
-        data_run = config.get_data_run(config.data_dir) if need_data_run else None
+        data_run = config.get_data_run(config.user_cfg.data_dir) if need_data_run else None
         config._update_relative_paths(sugar, current_run, data_run)
 
         return config
@@ -57,32 +67,32 @@ class Config(BaseModel):
             data_run = current_run
 
         path_to_logfile = f"ligand_sort/{data_run}/{data_run}.log"
-        self.sugar_binding_patterns_dir = self.data_dir / f"{data_run}/sugar_binding_patterns"
-        self.components_dir = self.data_dir / f"{data_run}/components"
-        self.mmcif_files_dir = self.data_dir / f"{data_run}/mmcif_files"
-        self.no_o6_mmcif_dir = self.data_dir / f"{data_run}/no_o6_mmcif"
-        self.validation_files_dir = self.data_dir / f"{data_run}/validation_files"
-        self.categorization_dir = self.results_dir / f"ligand_sort/{data_run}/categorization"
-        self.validation_dir = self.results_dir / f"ligand_sort/{data_run}/validation"
-        self.mv_run_dir = self.results_dir / f"ligand_sort/{data_run}/mv_run"
-        self.graph_analysis_dir = self.results_dir / f"ligand_sort/{data_run}/graph_analysis"
-        self.residue_graphs_dir = self.images_dir / f"ligands/{data_run}/residue_graphs"
+        self.sugar_binding_patterns_dir = self.user_cfg.data_dir / f"{data_run}/sugar_binding_patterns"
+        self.components_dir = self.user_cfg.data_dir / f"{data_run}/components"
+        self.mmcif_files_dir = self.user_cfg.data_dir / f"{data_run}/mmcif_files"
+        self.no_o6_mmcif_dir = self.user_cfg.data_dir / f"{data_run}/no_o6_mmcif"
+        self.validation_files_dir = self.user_cfg.data_dir / f"{data_run}/validation_files"
+        self.categorization_dir = self.user_cfg.results_dir / f"ligand_sort/{data_run}/categorization"
+        self.validation_dir = self.user_cfg.results_dir / f"ligand_sort/{data_run}/validation"
+        self.mv_run_dir = self.user_cfg.results_dir / f"ligand_sort/{data_run}/mv_run"
+        self.graph_analysis_dir = self.user_cfg.results_dir / f"ligand_sort/{data_run}/graph_analysis"
+        self.residue_graphs_dir = self.user_cfg.images_dir / f"ligands/{data_run}/residue_graphs"
 
         # Sugars are always newer than the data run, since they are downloaded by the second part of the process
-        self.sugars_dir = self.data_dir / f"{data_run}/sugars"
+        self.sugars_dir = self.user_cfg.data_dir / f"{data_run}/sugars"
 
         if sugar is not None:
             path_to_logfile = f"motif_based_search/{sugar}/{current_run}/{current_run}.log"
-            self.pq_run_dir = self.results_dir / f"motif_based_search/{sugar}/{current_run}/pq_run"
-            self.raw_binding_sites_dir = self.results_dir / f"motif_based_search/{sugar}/{current_run}/raw_binding_sites"
-            self.filtered_binding_sites_dir = self.results_dir / f"motif_based_search/{sugar}/{current_run}/filtered_binding_sites"
-            self.clusters_dir = self.results_dir / f"motif_based_search/{sugar}/{current_run}/clusters"
-            self.structure_motif_search_dir = self.results_dir / f"motif_based_search/{sugar}/{current_run}/structure_motif_search"
-            self.dendrograms_dir = self.images_dir / f"binding_sites/{sugar}/{current_run}/dendrograms"
-            self.tanglegrams_dir = self.images_dir / f"binding_sites/{sugar}/{current_run}/tanglegrams"
+            self.pq_run_dir = self.user_cfg.results_dir / f"motif_based_search/{sugar}/{current_run}/pq_run"
+            self.raw_binding_sites_dir = self.user_cfg.results_dir / f"motif_based_search/{sugar}/{current_run}/raw_binding_sites"
+            self.filtered_binding_sites_dir = self.user_cfg.results_dir / f"motif_based_search/{sugar}/{current_run}/filtered_binding_sites"
+            self.clusters_dir = self.user_cfg.results_dir / f"motif_based_search/{sugar}/{current_run}/clusters"
+            self.structure_motif_search_dir = self.user_cfg.results_dir / f"motif_based_search/{sugar}/{current_run}/structure_motif_search"
+            self.dendrograms_dir = self.user_cfg.images_dir / f"binding_sites/{sugar}/{current_run}/dendrograms"
+            self.tanglegrams_dir = self.user_cfg.images_dir / f"binding_sites/{sugar}/{current_run}/tanglegrams"
 
 
-        self.log_path = self.results_dir / path_to_logfile
+        self.log_path = self.user_cfg.results_dir / path_to_logfile
 
     
     @classmethod
