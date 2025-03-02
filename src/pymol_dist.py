bf82d4787037f4044fc1c9aab3fb51a331230fc8
Kateřina Nazarčuková <katka.nazarcukova@gmail.com>
2025-03-02 15:02:47 +0100

    WIP: sorted residue distances in PyMOL

--- src/pymol_dist.py
+++ src/pymol_dist.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from pymol import cmd
+from typing import Dict, Tuple, List
 
 
 def foo(max_res: int, path_to_file: Path):
@@ -18,23 +19,36 @@ def foo(max_res: int, path_to_file: Path):
     if len(chain) > 1:
         chain = chain[0]
 
+    sugar_name = res
     sugar = f"/{filename}//{chain}/{res}`{num}"
     cmd.select("sugar", sugar)
     
     if count > max_res:
-        distances = []
-        cmd.iterate("n. CA and polymer", "distances.append((resi, resn))", space=locals())
+        residues: List[Tuple[str, str]] = []
+        distances: Dict[Tuple[str, str], float] = {}
+        cmd.iterate("n. CA and polymer", "residues.append((resi, resn))",
+                    space=locals())
         
-        for resi, resn in distances:
+        for resi, resn in residues:
             current_residue = f"resi {resi} and name CA"
             # distance_name = f"dist_{resi}"
             sugar_atom = "sugar and name C1"
 
             # cmd.distance(f"dist_{resi}", current_residue, sugar_atom)
             
-            distance_value = cmd.get_distance(current_residue, sugar_atom)
+            distance_value: float = cmd.get_distance(current_residue, sugar_atom) # In Angstroms [Å]
+            # print(type(resi))
+            # print(type(resn))
+            distances[resn, resi] = distance_value
+            # print(type(distance_value))
 
-            print(f"Distance between residue {resn} ({resi}) and sugar: {distance_value} Å")
+
+
+            # print(f"Distance between residue {resn} ({resi}) and {sugar_name}: {distance_value} Å")
+        # print(type(residues))
+        # print(distances)
+        sorted_distances = dict(sorted(distances.items(), key=lambda item: item[1]))
+        print(sorted_distances)
 
     # return distances
 
@@ -45,3 +59,7 @@ if __name__ == "__main__":
     foo(10, Path("/home/kaci/dp/tmp/pymol_test/19_1gzt_FUC_201_A.pdb"))
     foo(10, Path("/home/kaci/dp/tmp/pymol_test/63_3zzv_FUC_3_E.pdb"))
     foo(10, Path("/home/kaci/dp/tmp/pymol_test/103_3uet_FUC_3_C.pdb"))
+    foo(10, Path("/home/kaci/dp/tmp/pymol_test/3_1bos_GAL_2_0.pdb"))
+    foo(10, Path("/home/kaci/dp/tmp/pymol_test/4_1bos_GAL_2_2.pdb"))
+    foo(10, Path("/home/kaci/dp/tmp/pymol_test/51_1cr7_GAL_2_K.pdb"))
+    foo(10, Path("/home/kaci/dp/tmp/pymol_test/58_3leg_FUC_4_B.pdb"))
