7d5960450bafbed79f0332579c929b0eeca7f63e
Kateřina Nazarčuková <katka.nazarcukova@gmail.com>
2025-03-02 20:55:03 +0100

    WIP: implemented maximum of 10 residues

--- src/pymol_dist.py
+++ src/pymol_dist.py
@@ -25,7 +25,7 @@ def foo(max_res: int, path_to_file: Path):
     
     if count > max_res:
         residues: List[Tuple[str, str]] = []
-        distances: Dict[Tuple[str, str], float] = {}
+        distances: List[Tuple[Tuple[str, str], float]] = []
         cmd.iterate("n. CA and polymer", "residues.append((resi, resn))",
                     space=locals())
         
@@ -39,7 +39,7 @@ def foo(max_res: int, path_to_file: Path):
             distance_value: float = cmd.get_distance(current_residue, sugar_atom) # In Angstroms [Å]
             # print(type(resi))
             # print(type(resn))
-            distances[resn, resi] = distance_value
+            distances.append(((resn, resi), distance_value))
             # print(type(distance_value))
 
 
@@ -47,19 +47,33 @@ def foo(max_res: int, path_to_file: Path):
             # print(f"Distance between residue {resn} ({resi}) and {sugar_name}: {distance_value} Å")
         # print(type(residues))
         # print(distances)
-        sorted_distances = dict(sorted(distances.items(), key=lambda item: item[1]))
-        print(sorted_distances)
+        sorted_distances = sorted(distances, key=lambda item: item[1])
+        cutoff_start = max_res # Structure Motif Search can take maximum of 10 residues
+        residues_to_remove = sorted_distances[cutoff_start:]
+        print(residues_to_remove)
 
-    # return distances
+        for (resn, resi), _ in residues_to_remove:
+            cmd.remove(f"resi {resi}")
+            cmd.save(f"/home/kaci/dp/tmp/pymol_test/max_10_res/{filename}.pdb")
 
+def test(path_to_file: Path):
+    cmd.delete("all")
+    cmd.load(path_to_file)
+    count = cmd.count_atoms("n. CA and polymer")
+    print(f"File {path_to_file.name}: {count} residues")
 
 
 if __name__ == "__main__":
-    foo(10, Path("/home/kaci/dp/tmp/pymol_test/15_6t9a_FUC_101_A.pdb"))
-    foo(10, Path("/home/kaci/dp/tmp/pymol_test/19_1gzt_FUC_201_A.pdb"))
-    foo(10, Path("/home/kaci/dp/tmp/pymol_test/63_3zzv_FUC_3_E.pdb"))
-    foo(10, Path("/home/kaci/dp/tmp/pymol_test/103_3uet_FUC_3_C.pdb"))
-    foo(10, Path("/home/kaci/dp/tmp/pymol_test/3_1bos_GAL_2_0.pdb"))
-    foo(10, Path("/home/kaci/dp/tmp/pymol_test/4_1bos_GAL_2_2.pdb"))
-    foo(10, Path("/home/kaci/dp/tmp/pymol_test/51_1cr7_GAL_2_K.pdb"))
-    foo(10, Path("/home/kaci/dp/tmp/pymol_test/58_3leg_FUC_4_B.pdb"))
+    # foo(10, Path("/home/kaci/dp/tmp/pymol_test/15_6t9a_FUC_101_A.pdb"))
+    test(Path("/home/kaci/dp/tmp/pymol_test/max_10_res/51_1cr7_GAL_2_K.pdb"))
+    test(Path("/home/kaci/dp/tmp/pymol_test/max_10_res/63_3zzv_FUC_3_E.pdb"))
+    test(Path("/home/kaci/dp/tmp/pymol_test/max_10_res/103_3uet_FUC_3_C.pdb"))
+    test(Path("/home/kaci/dp/tmp/pymol_test/max_10_res/15_6t9a_FUC_101_A.pdb"))
+    test(Path("/home/kaci/dp/tmp/pymol_test/max_10_res/19_1gzt_FUC_201_A.pdb"))
+    # foo(10, Path("/home/kaci/dp/tmp/pymol_test/19_1gzt_FUC_201_A.pdb"))
+    # foo(10, Path("/home/kaci/dp/tmp/pymol_test/63_3zzv_FUC_3_E.pdb"))
+    # foo(10, Path("/home/kaci/dp/tmp/pymol_test/103_3uet_FUC_3_C.pdb"))
+    # foo(10, Path("/home/kaci/dp/tmp/pymol_test/3_1bos_GAL_2_0.pdb"))
+    # foo(10, Path("/home/kaci/dp/tmp/pymol_test/4_1bos_GAL_2_2.pdb"))
+    # foo(10, Path("/home/kaci/dp/tmp/pymol_test/51_1cr7_GAL_2_K.pdb"))
+    # foo(10, Path("/home/kaci/dp/tmp/pymol_test/58_3leg_FUC_4_B.pdb"))
