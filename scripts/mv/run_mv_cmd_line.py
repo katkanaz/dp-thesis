68c41f03a9bdbf9e69984275c6f920350728af51
Kateřina Nazarčuková <katka.nazarcukova@gmail.com>
2025-05-02 01:20:36 +0200

    feat: rum MV and PQ from cmd line

---
+++ scripts/mv/run_mv_cmd_line.py
@@ -0,0 +1,53 @@
+import json
+from subprocess import Popen, PIPE
+from platform import system
+from datetime import datetime
+from pathlib import Path
+
+
+def create_mv_config(run_dir: Path) -> None:
+    print("Creating config file")
+
+    mv_config = {
+        "ValidationType": "Sugars",
+        "InputFolder":  "../../debug/mv_altlocs/read_write_test/",
+        "ModelsSource": "../mv/components_sugars_only.cif",
+        "IsModelsSourceComponentDictionary": True,
+        "IgnoreObsoleteComponentDictionaryEntries": False,
+        "SummaryOnly": False,
+        "DatabaseModeMinModelAtomCount": 0,
+        "DatabaseModeIgnoreNames": [],
+        "MaxDegreeOfParallelism": 8
+    }
+
+    with open(f"{run_dir}/mv_config.json", "w", encoding="utf8") as f:
+        json.dump(mv_config, f, indent=4)
+    
+
+def run_mv(is_unix: bool, run_dir: Path) -> None:
+    create_mv_config(run_dir)
+
+    cmd = [f"{'mono ' if is_unix is True else ''}"
+           "../../mv/MotiveValidator/WebChemistry.MotiveValidator.Service.exe "
+           f"{run_dir}/results "
+           f"{run_dir}/mv_config.json"]
+
+    with Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True, text=True) as mv_proc:
+        assert mv_proc.stdout is not None, "stdout is set to PIPE in Popen"
+        for line in mv_proc.stdout:
+            print(f"STDOUT: {line.strip()}")
+        assert mv_proc.stderr is not None, "stderr is set to PIPE in Popen"
+        for line in mv_proc.stderr:
+            print(f"STDERR: {line.strip()}")
+
+    if mv_proc.returncode != 0:
+        print(f"MV process exited with code {mv_proc.returncode}")
+    else:
+        print("MV process completed successfully")
+
+if __name__ == "__main__":
+    is_unix = system() != "Windows"
+    Path(f"../../debug/mv_altlocs/mv_{datetime.now().strftime('%Y-%m-%dT%H-%M-%S')}").mkdir(exist_ok=True, parents=True)
+    run_dir = Path(f"../../debug/mv_altlocs/mv_{datetime.now().strftime('%Y-%m-%dT%H-%M-%S')}")
+
+    run_mv(is_unix, run_dir)
---
+++ scripts/pq/run_pq_cmd_line.py
@@ -0,0 +1,78 @@
+from argparse import ArgumentParser
+import json
+from subprocess import Popen, PIPE
+from pathlib import Path
+from platform import system
+from datetime import datetime
+
+
+def create_pq_config(sugar: str, run_dir: Path) -> None:
+    print("Creating config file")
+
+    pq_config = {
+        "InputFolders": [
+            "../../debug/mv_altlocs/all_altlocs_for_mv/",
+        ],
+        "Queries": [],
+        "StatisticsOnly": False,
+        "MaxParallelism": 2
+    }
+
+    structure = "A_1aa5"
+    residues = [{
+                "name": "NAG",
+                "num": "1",
+                "chain": "C"
+            }]
+
+    case_sensitive_check = []
+    for residue in residues:
+        if residue["name"] == sugar:
+            case = f"{residue['num']}_{residue['chain']}"
+            if case.upper() in case_sensitive_check or case.lower() in case_sensitive_check:
+                query_id = f"{structure}_{residue['name']}_{residue['num']}_{residue['chain']}_2"
+            else:
+                case_sensitive_check.append(case)
+                query_id = f"{structure}_{residue['name']}_{residue['num']}_{residue['chain']}"
+            query_str = f"ResidueIds('{residue['num']} {residue['chain']}').AmbientResidues(5)"
+            pq_config["Queries"].append({"Id": query_id, "QueryString": query_str})
+
+    with open(f"{run_dir}/pq_config.json", "w") as f:
+        json.dump(pq_config, f, indent=4)
+
+
+def run_pq(sugar: str, is_unix: bool, run_dir: Path) -> None:
+    create_pq_config(sugar, run_dir)
+
+    cmd = [f"{'mono ' if is_unix is True else ''}"
+           "../../pq/PatternQuery/WebChemistry.Queries.Service.exe "
+           f"{run_dir}/results "
+           f"{run_dir}/pq_config.json"]
+
+
+    with Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True, text=True) as pq_proc:
+        assert pq_proc.stdout is not None, "stdout is set to PIPE in Popen" 
+        for line in pq_proc.stdout:
+            print(f"STDOUT: {line.strip()}")
+        assert pq_proc.stderr is not None, "stderr is set to PIPE in Popen" 
+        for line in pq_proc.stderr:
+            print(f"STDERR: {line.strip()}")
+
+    if pq_proc.returncode != 0:
+        print(f"PQ process exited with code {pq_proc.returncode}")
+    else:
+        print("PQ process completed successfully")
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+
+    parser.add_argument("-s", "--sugar", help="Three letter code of sugar", type=str, required=True)
+
+    args = parser.parse_args()
+
+    is_unix = system() != "Windows"
+
+    run_dir = Path(f"../../debug/mv_altlocs/pq_{datetime.now().strftime('%Y-%m-%dT%H-%M-%S')}")
+    Path(f"../../debug/mv_altlocs/pq_{datetime.now().strftime('%Y-%m-%dT%H-%M-%S')}").mkdir(exist_ok=True, parents=True)
+    run_pq(args.sugar, is_unix, run_dir)
