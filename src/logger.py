5b4d594c19e3414d1b53328b8bb0b7e3884ff13b
Kateřina Nazarčuková <katka.nazarcukova@gmail.com>
2025-05-30 17:03:27 +0200

    feat: color added to logging console output

--- src/logger.py
+++ src/logger.py
@@ -10,6 +10,24 @@ class CrashHandler(logging.Handler):
         raise RuntimeError(self.format(record))
 
 
+class ColoredFormatter(logging.Formatter):
+    COLORS = {
+        "DEBUG": "\033[94m",
+        "INFO": "\033[92m",
+        "WARNING": "\033[93m",
+        "ERROR": "\033[91m",
+        "CRITICAL": "\033[95m",
+        "RESET": "\033[0m"
+    }
+
+    def format(self, record):
+        color = self.COLORS.get(record.levelname, self.COLORS["RESET"])
+        reset = self.COLORS["RESET"]
+        record.levelname = f"{color}{record.levelname}{reset}"
+        record.msg = f"{color}{record.msg}{reset}"
+        return super().format(record)
+
+
 class TqdmLoggingHandler(logging.Handler):
     def __init__(self, level=logging.NOTSET):
         super().__init__(level)
@@ -39,16 +57,16 @@ def setup_logger(log_file: Path, log_level=logging.INFO):
 
     logger.setLevel(log_level)
 
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(filename)s - %(funcName)s - %(message)s")
+    formatting_str = "%(asctime)s - %(name)s - %(levelname)s - %(filename)s - %(funcName)s - %(message)s"
 
     log_file.parent.mkdir(exist_ok=True, parents=True)
     file_handler = logging.FileHandler(log_file)
     file_handler.setLevel(log_level)
-    file_handler.setFormatter(formatter)
+    file_handler.setFormatter(logging.Formatter(formatting_str))
 
     console_handler = TqdmLoggingHandler()
     console_handler.setLevel(log_level)
-    console_handler.setFormatter(formatter)
+    console_handler.setFormatter(ColoredFormatter(formatting_str))
 
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
