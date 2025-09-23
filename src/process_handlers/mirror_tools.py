c9a6e7fe66b2b0a9f4286be028a812a899cc0c47
Kateřina Nazarčuková <katka.nazarcukova@gmail.com>
2025-09-23 22:10:00 +0200

    chore: add docs

--- src/process_handlers/mirror_tools.py
+++ src/process_handlers/mirror_tools.py
@@ -25,7 +25,12 @@ def get_pdb_ids_from_pq(src_path: Path, result_file: Path) -> None:
 
 
 def get_pq_result(config: Config) -> None:
-    # TODO: Add docs
+    """
+    Retrieve PatternQuery result archive from the remote host via rsync.
+
+    :param config: Config object
+    """
+
     user = os.getenv("RSYNC_USER")
     assert user is not None, "RSYNC_USER is missing from environment"
     password = os.getenv("RSYNC_PASSWORD")
@@ -36,6 +41,7 @@ def get_pq_result(config: Config) -> None:
     src_path = f"{user}@{host}:/storage/brno2/home/{user}/pq-runs/2025-07-19T17-44-results/result/result.zip" # FIXME: Not abstract enough 
     dest_path = Path(config.sugar_binding_patterns_dir)
 
+    logger.info("Downloading PQ results")
 
     cmd = [
         "/usr/bin/rsync",
@@ -45,18 +51,32 @@ def get_pq_result(config: Config) -> None:
         dest_path
     ]
 
-    subprocess.run(cmd, check=True)
+    subprocess.run(cmd, check=True) # TODO: add runtimeerror
 
 
 def create_file_list(config: Config, pdb_ids: set[str]) -> None:
-    # TODO: Add docs
+    """
+    Create a text file containing the IDs of the structures to download for rsync.
+
+    :param config: Config object
+    :param pdb_ids: IDs of structures to be written in the file
+    """
+
+    logger.info("Creating file list for rsync")
+
     with open(config.run_data_dir / "file_list.txt", "w", encoding="utf8") as f:
         for pdb_id in pdb_ids:
             f.write(f"{pdb_id}.cif.gz\n")
 
 
 def download_structures_from_mirror(dest_path: Path, file_list_path: Path) -> None:
-    # TODO: Add docs
+    """
+    Retrieve mmCIF structure files from the remote host via rsync.
+
+    :param dest_path: Download destination directory
+    :param file_list_path: Path to a text file containing the IDs of the structures to download
+    """
+
     user = os.getenv("RSYNC_USER")
     assert user is not None, "RSYNC_USER is missing from environment"
     password = os.getenv("RSYNC_PASSWORD")
@@ -66,7 +86,7 @@ def download_structures_from_mirror(dest_path: Path, file_list_path: Path) -> No
 
     src_path = f"{user}@{host}:/storage/brno2/home/{user}/pdb-mirror/"
 
-    logger.info("Downloading validation files")
+    logger.info("Downloading structures files")
 
     cmd = [
         "/usr/bin/rsync",
@@ -77,4 +97,4 @@ def download_structures_from_mirror(dest_path: Path, file_list_path: Path) -> No
         dest_path
     ]
 
-    subprocess.run(cmd, check=True)
+    subprocess.run(cmd, check=True) # TODO: add runtimeerror
