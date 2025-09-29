843dee158226c09c6ed0609f96f9fa7ab18cceff
Kateřina Nazarčuková <katka.nazarcukova@gmail.com>
2025-09-29 22:13:23 +0200

    chore: change types for python 3.8

--- scripts/analyze_something_went_wrong.py
+++ scripts/analyze_something_went_wrong.py
@@ -1,12 +1,12 @@
-import collections
 import json
 from pathlib import Path
 from collections import Counter
+from typing import List, Counter
 
 
 def analyze_something_went_wrong(path_to_file: Path) -> Counter:
     with open(path_to_file, "r", encoding="utf8") as file:
-        wrong: list[list[str]] = json.load(file)
+        wrong: List[List[str]] = json.load(file)
 
     problematic = Counter(string for sublist in wrong for string in sublist)
 
@@ -18,14 +18,9 @@ def analyze_something_went_wrong(path_to_file: Path) -> Counter:
     return problematic
 
 # How many surroundings from which structure
-def get_pdb_ids_of_problematic(counter: collections.Counter[str]) -> Counter:
+def get_pdb_ids_of_problematic(counter: Counter[str]) -> Counter:
     pdb_ids = Counter(key.split("_")[2] for key in counter.keys())
 
     print(pdb_ids)
     return(pdb_ids)
 
-
-
-
-# get_pdb_ids_of_problematic(analyze_something_went_wrong(Path("/home/kaci/tmp/count_test.json")))
-get_pdb_ids_of_problematic(analyze_something_went_wrong(Path("/home/kaci/dp/results/motif_based_search/GLC/2025-05-30T16-52-00/clusters/something_wrong.json")))
