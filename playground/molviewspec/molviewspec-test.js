f86b6280d8dfd988266dbf894aa3325c863511b9
Kateřina Nazarčuková <katka.nazarcukova@gmail.com>
2025-11-04 12:16:47 +0100

    WIP: simple visualization of June FUC search results

--- playground/molviewspec/molviewspec-test.js
+++ playground/molviewspec/molviewspec-test.js
@@ -1,12 +1,78 @@
-const builder = molstar.PluginExtensions.mvs.MVSData.createBuilder();
-const structure = builder
-    .download({ url: "https://models.rcsb.org/af_afa0a175w077f1.bcif" })
-    .parse({ format: 'bcif' })
-    .modelStructure({})
-    .component({})
-    .representation({})
-    .color({ color: "blue" });
-const mvsData = builder.getState();
 
-molstar.Viewer.create("viewer1", { layoutIsExpanded: false, layoutShowControls: false })
-    .then(viewer => molstar.PluginExtensions.mvs.loadMVS(viewer.plugin, mvsData, { sourceUrl: undefined, sanityChecks: true, replaceExisting: false }));
+console.log(searchResults);
+
+const containerOuter = document.getElementById("container-outer");
+
+for (representativeSurrounding in searchResults) {
+    const detailEl = document.createElement("details");
+    const summaryEl = document.createElement("summary");
+
+
+    const summaryDiv = document.createElement("div");
+    summaryDiv.style.display = "inline-flex";
+    summaryDiv.style.justifyContent = "space-between";
+    summaryDiv.style.width = "30%"
+
+    const surroundingNameDiv = document.createElement("div"); 
+    surroundingNameDiv.appendChild(document.createTextNode(representativeSurrounding));
+    summaryDiv.appendChild(surroundingNameDiv);
+
+    const numFoundDiv = document.createElement("div"); 
+    numFoundDiv.appendChild(document.createTextNode(`${Object.keys(searchResults[representativeSurrounding]).length} found`));
+    summaryDiv.appendChild(numFoundDiv);
+
+    summaryEl.appendChild(summaryDiv);
+
+
+    detailEl.appendChild(summaryEl);
+
+
+    const computedModelCardContainer = document.createElement("div");
+
+    for (computedModelId in searchResults[representativeSurrounding]) {
+        const computedModelCard = document.createElement("div");
+        const computedModelCardLeft = document.createElement("div");
+        const computedModelCardRight = document.createElement("div");
+
+        const computedModelName = document.createElement("div");
+        computedModelName.appendChild(document.createTextNode(computedModelId));
+        const computedModelTitle = document.createElement("div");
+        computedModelTitle.appendChild(document.createTextNode(searchResults[representativeSurrounding][computedModelId][0]));
+        const computedModelKeyWords = document.createElement("div");
+        computedModelKeyWords.appendChild(document.createTextNode(searchResults[representativeSurrounding][computedModelId][1]));
+
+        computedModelCard.appendChild(computedModelCardLeft);
+        computedModelCard.appendChild(computedModelCardRight);
+
+        computedModelCardRight.appendChild(computedModelName);
+        computedModelCardRight.appendChild(computedModelTitle);
+        computedModelCardRight.appendChild(computedModelKeyWords);
+
+        const molViewSpecDiv = document.createElement("div");
+        molViewSpecDiv.style.width = "200px";
+        molViewSpecDiv.style.height = "200px";
+        molViewSpecDiv.style.position = "relative";
+
+        const builder = molstar.PluginExtensions.mvs.MVSData.createBuilder();
+        const structure = builder
+            .download({ url: "https://models.rcsb.org/af_afa0a175w077f1.bcif" })
+            .parse({ format: 'bcif' })
+            .modelStructure({})
+            .component({})
+            .representation({})
+            .color({ color: "blue" });
+        const mvsData = builder.getState();
+
+        molstar.Viewer.create(molViewSpecDiv, { layoutIsExpanded: false, layoutShowControls: false })
+            .then(viewer => molstar.PluginExtensions.mvs.loadMVS(viewer.plugin, mvsData, { sourceUrl: undefined, sanityChecks: true, replaceExisting: false }));
+        
+        computedModelCardLeft.appendChild(molViewSpecDiv);
+
+
+        computedModelCardContainer.appendChild(computedModelCard);
+    }
+
+    detailEl.appendChild(computedModelCardContainer);
+
+    containerOuter.appendChild(detailEl);
+}
---
+++ playground/molviewspec/search_results.js
@@ -0,0 +1,474 @@
+const searchResults = {
+    "0_0_2nzy_FUC_4001_A": {
+        "AF_AFO25142F1": [
+            "Computed structure model of Alpha-(1,3)-fucosyltransferase",
+            "No keywords"
+        ],
+        "AF_AFO25366F1": [
+            "Computed structure model of Alpha-(1,3)-fucosyltransferase",
+            "No keywords"
+        ],
+        "AF_AFO30511F1": [
+            "Computed structure model of Alpha-(1,3)-fucosyltransferase FucT",
+            "No keywords"
+        ]
+    },
+    "104_0_5t54_FUC_2_C": {
+        "AF_AFA0A0D2HKQ5F1": [
+            "Computed structure model of Unplaced genomic scaffold supercont1.1, whole genome shotgun sequence",
+            "No keywords"
+        ],
+        "AF_AFA0A0K0EH67F1": [
+            "Computed structure model of Uncharacterized protein",
+            "No keywords"
+        ],
+        "AF_AFA0A1C1CMQ4F1": [
+            "Computed structure model of Isochorismatase domain-containing protein",
+            "No keywords"
+        ],
+        "AF_AFC0NW69F1": [
+            "Computed structure model of Vacuolar sorting-associated protein",
+            "No keywords"
+        ],
+        "AF_AFF1QPX5F1": [
+            "Computed structure model of Contactin-associated protein 1",
+            "No keywords"
+        ],
+        "AF_AFO29524F1": [
+            "Computed structure model of Ribosomal RNA small subunit methyltransferase Nep1",
+            "No keywords"
+        ],
+        "AF_AFO33333F1": [
+            "Computed structure model of Probable transposase",
+            "No keywords"
+        ],
+        "AF_AFP86993F1": [
+            "Computed structure model of Lectin",
+            "No keywords"
+        ],
+        "AF_AFP94531F1": [
+            "Computed structure model of Intracellular exo-alpha-(1->5)-L-arabinofuranosidase 1",
+            "No keywords"
+        ],
+        "AF_AFQ0CTQ7F1": [
+            "Computed structure model of Probable arabinogalactan endo-beta-1,4-galactanase A",
+            "No keywords"
+        ],
+        "AF_AFQ581A4F1": [
+            "Computed structure model of Uncharacterized protein",
+            "No keywords"
+        ],
+        "AF_AFQ5AKY0F1": [
+            "Computed structure model of Kis2p",
+            "No keywords"
+        ],
+        "AF_AFQ9KBR4F1": [
+            "Computed structure model of Intracellular exo-alpha-(1->5)-L-arabinofuranosidase",
+            "No keywords"
+        ]
+    },
+    "115_0_7vs9_FUC_2_C": {},
+    "140_0_1qot_FUC_2_E": {
+        "AF_AFP22970F1": [
+            "Computed structure model of Anti-H(O) lectin 1",
+            "No keywords"
+        ],
+        "AF_AFP22973F1": [
+            "Computed structure model of Anti-H(O) lectin 2",
+            "No keywords"
+        ],
+        "AF_AFP23558F1": [
+            "Computed structure model of Lectin 1",
+            "No keywords"
+        ]
+    },
+    "159_0_2ec9_FUC_1060_L": {},
+    "15_0_2wmi_FUC_2_C": {
+        "AF_AFQ6RUF5F1": [
+            "Computed structure model of Blood-group-substance endo-1,4-beta-galactosidase",
+            "No keywords"
+        ]
+    },
+    "170_0_2j1v_FUC_3_C": {
+        "AF_AFQ8CY62F1": [
+            "Computed structure model of F5/8 type C domain-containing protein",
+            "No keywords"
+        ]
+    },
+    "1_0_4aha_FUC_2_F": {
+        "AF_AFQ4WW81F1": [
+            "Computed structure model of Fucose-specific lectin",
+            "No keywords"
+        ],
+        "AF_AFQ8NJT4F1": [
+            "Computed structure model of Fucose-specific lectin",
+            "No keywords"
+        ]
+    },
+    "210_0_6e0w_FUC_2_D": {},
+    "25_0_2jdm_FUC_1118_A": {},
+    "275_0_6zzw_FUC_5_D": {},
+    "27_0_4ah4_FUC_2_K": {
+        "AF_AFQ4WW81F1": [
+            "Computed structure model of Fucose-specific lectin",
+            "No keywords"
+        ],
+        "AF_AFQ8NJT4F1": [
+            "Computed structure model of Fucose-specific lectin",
+            "No keywords"
+        ]
+    },
+    "2_0_5elb_FUC_4_P": {},
+    "35_0_2wcv_FUC_1141_J": {},
+    "418_0_4c22_FUC_603_B": {
+        "AF_AFB1I9W8F1": [
+            "Computed structure model of L-fucose isomerase",
+            "No keywords"
+        ],
+        "AF_AFC1CND0F1": [
+            "Computed structure model of L-fucose isomerase",
+            "No keywords"
+        ],
+        "AF_AFQ04I16F1": [
+            "Computed structure model of L-fucose isomerase",
+            "No keywords"
+        ],
+        "AF_AFQ8DN24F1": [
+            "Computed structure model of L-fucose isomerase",
+            "No keywords"
+        ],
+        "AF_AFQ97N97F1": [
+            "Computed structure model of L-fucose isomerase",
+            "No keywords"
+        ]
+    },
+    "45_0_1mmx_FUC_348_B": {
+        "AF_AFP21955F1": [
+            "Computed structure model of Aldose 1-epimerase",
+            "No keywords"
+        ]
+    },
+    "4_0_3e6j_FUC_3_B": {},
+    "56_0_6orh_FUC_3_C": {
+        "AF_AFQ8CY64F1": [
+            "Computed structure model of F5/8 type C domain-containing protein",
+            "No keywords"
+        ]
+    },
+    "58_0_7q1w_FUC_3_E": {
+        "AF_AFO08601F1": [
+            "Computed structure model of Microsomal triglyceride transfer protein large subunit",
+            "No keywords"
+        ],
+        "AF_AFA0A1D6EAL8F1": [
+            "Computed structure model of DExH-box ATP-dependent RNA helicase DExH7 chloroplastic",
+            "No keywords"
+        ],
+        "AF_AFA0A2R8QQ02F1": [
+            "Computed structure model of VLIG-type G domain-containing protein",
+            "No keywords"
+        ],
+        "AF_AFQ9XYF6F1": [
+            "Computed structure model of Myosin-D",
+            "No keywords"
+        ],
+        "AF_AFA0A077Z618F1": [
+            "Computed structure model of UPF0184 domain containing protein",
+            "No keywords"
+        ],
+        "AF_AFA0A077Z8X7F1": [
+            "Computed structure model of Kinesin protein KIF14",
+            "No keywords"
+        ],
+        "AF_AFA0A0D2F0L0F1": [
+            "Computed structure model of Phosphoribosylaminoimidazole carboxylase",
+            "No keywords"
+        ],
+        "AF_AFA0A0H2ZJS2F1": [
+            "Computed structure model of Diguanylate cyclase DgcP",
+            "No keywords"
+        ],
+        "AF_AFA0A0H5S5Q5F1": [
+            "Computed structure model of Bm7435",
+            "No keywords"
+        ],
+        "AF_AFA0A0K0JXE1F1": [
+            "Computed structure model of Aurora kinase",
+            "No keywords"
+        ],
+        "AF_AFA0A0P0Y561F1": [
+            "Computed structure model of Os11g0619500 protein",
+            "No keywords"
+        ],
+        "AF_AFA0A0S2Z669F1": [
+            "Computed structure model of Testis expressed 9 isoform 1",
+            "No keywords"
+        ],
+        "AF_AFA0A1D6JMA9F1": [
+            "Computed structure model of DNA binding protein",
+            "No keywords"
+        ],
+        "AF_AFA0A1D6NKH3F1": [
+            "Computed structure model of Actin binding protein family",
+            "No keywords"
+        ],
+        "AF_AFA0A3Q0KDP6F1": [
+            "Computed structure model of Uncharacterized protein",
+            "No keywords"
+        ],
+        "AF_AFA0A5K4FDD8F1": [
+            "Computed structure model of Uncharacterized protein",
+            "No keywords"
+        ],
+        "AF_AFA1S1N6F1": [
+            "Computed structure model of Imidazolonepropionase",
+            "No keywords"
+        ],
+        "AF_AFA3DAF6F1": [
+            "Computed structure model of Imidazolonepropionase",
+            "No keywords"
+        ],
+        "AF_AFA3QJH7F1": [
+            "Computed structure model of Imidazolonepropionase",
+            "No keywords"
+        ],
+        "AF_AFA4I4Q6F1": [
+            "Computed structure model of Uncharacterized protein",
+            "No keywords"
+        ],
+        "AF_AFA5F1X8F1": [
+            "Computed structure model of Imidazolonepropionase",
+            "No keywords"
+        ],
+        "AF_AFA6WHH8F1": [
+            "Computed structure model of Imidazolonepropionase",
+            "No keywords"
+        ],
+        "AF_AFA8G1S8F1": [
+            "Computed structure model of Imidazolonepropionase",
+            "No keywords"
+        ],
+        "AF_AFA9KV78F1": [
+            "Computed structure model of Imidazolonepropionase",
+            "No keywords"
+        ],
+        "AF_AFB3ENZ9F1": [
+            "Computed structure model of Serine--tRNA ligase",
+            "No keywords"
+        ],
+        "AF_AFB4F0M6F1": [
+            "Computed structure model of Lysine--tRNA ligase",
+            "No keywords"
+        ],
+        "AF_AFB9DGK6F1": [
+            "Computed structure model of AT2G44210 protein",
+            "No keywords"
+        ],
+        "AF_AFC3LLQ1F1": [
+            "Computed structure model of Imidazolonepropionase",
+            "No keywords"
+        ],
+        "AF_AFC6DH76F1": [
+            "Computed structure model of Glycogen synthase",
+            "No keywords"
+        ],
+        "AF_AFD3Z951F1": [
+            "Computed structure model of Rho GTPase-activating protein 11A",
+            "No keywords"
+        ],
+        "AF_AFF1RDR9F1": [
+            "Computed structure model of Microtubule associated scaffold protein 2",
+            "No keywords"
+        ],
+        "AF_AFI1LLX5F1": [
+            "Computed structure model of BZIP domain-containing protein",
+            "No keywords"
+        ],
+        "AF_AFI1LPW5F1": [
+            "Computed structure model of BZIP domain-containing protein",
+            "No keywords"
+        ],
+        "AF_AFK7WHC4F1": [
+            "Computed structure model of BZIP transcription factor",
+            "No keywords"
+        ],
+        "AF_AFO28044F1": [
+            "Computed structure model of Uncharacterized protein AF_2239",
+            "No keywords"
+        ],
+        "AF_AFO58938F1": [
+            "Computed structure model of CRISPR-associated endonuclease Cas1",
+            "No keywords"
+        ],
+        "AF_AFO62484F1": [
+            "Computed structure model of Uncharacterized protein",
+            "No keywords"
+        ],
+        "AF_AFP47251F1": [
+            "Computed structure model of Serine--tRNA ligase",
+            "No keywords"
+        ],
+        "AF_AFP55157F1": [
+            "Computed structure model of Microsomal triglyceride transfer protein large subunit",
+            "No keywords"
+        ],
+        "AF_AFQ0P929F1": [
+            "Computed structure model of Putative two-component regulator",
+            "No keywords"
+        ],
+        "AF_AFQ18A79F1": [
+            "Computed structure model of Glycogen synthase",
+            "No keywords"
+        ],
+        "AF_AFQ38AB3F1": [
+            "Computed structure model of Flagellum transition zone component",
+            "No keywords"
+        ],
+        "AF_AFQ47G58F1": [
+            "Computed structure model of Urease accessory protein UreG",
+            "No keywords"
+        ],
+        "AF_AFQ4CPG0F1": [
+            "Computed structure model of Lactamase_B domain-containing protein",
+            "No keywords"
+        ],
+        "AF_AFQ4DJ16F1": [
+            "Computed structure model of cwf21 domain-containing protein",
+            "No keywords"
+        ],
+        "AF_AFQ54JJ4F1": [
+            "Computed structure model of Uncharacterized protein",
+            "No keywords"
+        ],
+        "AF_AFQ54RM0F1": [
+            "Computed structure model of NuA4 complex subunit EAF3 homolog",
+            "No keywords"
+        ],
+        "AF_AFQ55D16F1": [
+            "Computed structure model of KIN17-like protein",
+            "No keywords"
+        ],
+        "AF_AFQ57UJ9F1": [
+            "Computed structure model of Uncharacterized protein",
+            "No keywords"
+        ],
+        "AF_AFQ582H8F1": [
+            "Computed structure model of Uncharacterized protein",
+            "No keywords"
+        ],
+        "AF_AFQ5M856F1": [
+            "Computed structure model of Kinetochore protein Spc25",
+            "No keywords"
+        ],
+        "AF_AFQ5ZDB2F1": [
+            "Computed structure model of Os01g0363900 protein",
+            "No keywords"
+        ],
+        "AF_AFQ6DIX6F1": [
+            "Computed structure model of BICD family-like cargo adapter 2",
+            "No keywords"
+        ],
+        "AF_AFQ6MQU0F1": [
+            "Computed structure model of tRNA (guanine-N(7)-)-methyltransferase",
+            "No keywords"
+        ],
+        "AF_AFQ6S4P4F1": [
+            "Computed structure model of Transcription factor RF2b",
+            "No keywords"
+        ],
+        "AF_AFQ7VRR0F1": [
+            "Computed structure model of Ribonuclease 3",
+            "No keywords"
+        ],
+        "AF_AFQ7XXC1F1": [
+            "Computed structure model of Os04g0181300 protein",
+            "No keywords"
+        ],
+        "AF_AFQ865F1F1": [
+            "Computed structure model of Microsomal triglyceride transfer protein large subunit",
+            "No keywords"
+        ],
+        "AF_AFQ8II01F1": [
+            "Computed structure model of Tudor staphylococcal nuclease",
+            "No keywords"
+        ],
+        "AF_AFQ8LN44F1": [
+            "Computed structure model of BZIP transcription factor family protein, expressed",
+            "No keywords"
+        ],
+        "AF_AFQ8N6V9F1": [
+            "Computed structure model of Testis-expressed protein 9",
+            "No keywords"
+        ],
+        "AF_AFQ9DA73F1": [
+            "Computed structure model of Coiled-coil domain-containing protein 89",
+            "No keywords"
+        ],
+        "AF_AFQ9H972F1": [
+            "Computed structure model of Uncharacterized protein C14orf93",
+            "No keywords"
+        ],
+        "AF_AFQ9HT84F1": [
+            "Computed structure model of Diguanylate cyclase DgcP",
+            "No keywords"
+        ],
+        "AF_AFQ9KSQ1F1": [
+            "Computed structure model of Imidazolonepropionase",
+            "No keywords"
+        ],
+        "AF_AFQ9RLT3F1": [
+            "Computed structure model of Protein CitXG",
+            "No keywords"
+        ],
+        "AF_AFQ9VHC8F1": [
+            "Computed structure model of LD31448p",
+            "No keywords"
+        ],
+        "AF_AFQ9WUJ3F1": [
+            "Computed structure model of Myomegalin",
+            "No keywords"
+        ]
+    },
+    "9_0_3uet_FUC_3_D": {
+        "AF_AFA0A175VWT6F1": [
+            "Computed structure model of Alpha-L-fucosidase",
+            "No keywords"
+        ],
+        "AF_AFA0A1D6E3G0F1": [
+            "Computed structure model of Alpha-L-fucosidase",
+            "No keywords"
+        ],
+        "AF_AFA0A1D6ETA0F1": [
+            "Computed structure model of Alpha-L-fucosidase",
+            "No keywords"
+        ],
+        "AF_AFC4J251F1": [
+            "Computed structure model of Alpha-L-fucosidase",
+            "No keywords"
+        ],
+        "AF_AFI1LDX9F1": [
+            "Computed structure model of Alpha-L-fucosidase",
+            "No keywords"
+        ],
+        "AF_AFI1NGM2F1": [
+            "Computed structure model of Alpha-L-fucosidase",
+            "No keywords"
+        ],
+        "AF_AFQ650T8F1": [
+            "Computed structure model of Alpha-L-fucosidase",
+            "No keywords"
+        ],
+        "AF_AFQ7XUR3F1": [
+            "Computed structure model of Putative alpha-L-fucosidase 1",
+            "No keywords"
+        ],
+        "AF_AFQ8CY64F1": [
+            "Computed structure model of F5/8 type C domain-containing protein",
+            "No keywords"
+        ],
+        "AF_AFQ8GW72F1": [
+            "Computed structure model of Alpha-L-fucosidase 1",
+            "No keywords"
+        ]
+    }
+}
