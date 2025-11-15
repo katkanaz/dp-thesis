ff97276e7d80ab770e8d61b950e6750f70537832
Kateřina Nazarčuková <katka.nazarcukova@gmail.com>
2025-11-15 16:03:59 +0100

    WIP: start of go backend

---
+++ web/be/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {w.Write([]byte("hello"))})
+
+	http.ListenAndServe(":8081", r)
+}
