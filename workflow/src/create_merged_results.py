2d23951888a682e4a4c16cbda38bdad687e798ec
Kateřina Nazarčuková <katka.nazarcukova@gmail.com>
2026-02-17 17:26:04 +0100

    feat: create merged results script

--- workflow/src/create_merged_results.py
+++ workflow/src/create_merged_results.py
@@ -2,20 +2,34 @@ from argparse import ArgumentParser
 import json
 from pathlib import Path
 
+from utils.parse_surrounding_name import parse_surrounding_name
 
-def main(source: list[Path], output: Path) -> None:
-    print(source)
-    results = {}
+
+def create_merged_results(source: list[Path], output: Path) -> None:
+    merged = {}
 
     for file_name in source:
         with open(file_name, "r") as f:
-            data: dict = json.load(f)
-            for surrounding, comp_structs in data.items():
-                for struct in comp_structs:
-                    if struct not in results:
-                        results[struct] = {
-
+            results: dict = json.load(f)
+            for surrounding, comp_structs in results.items():
+                for struct, data in comp_structs.items():
+                    if struct not in merged:
+                        merged[struct] = {
+                            "pdb_id": struct,
+                            "afdb_id": data["afdb_id"],
+                            "title": data["title"],
+                            "organism": data["organism"],
+                            "plddt": data["plddt"],
+                            "af_version": data["af_version"],
+                            "af_revision": data["af_revision"],
+                            "motifs": []
                         }
+                    surrounding_name = parse_surrounding_name(surrounding)
+                    motif_metadata = {"surrounding": surrounding, "sugar": surrounding_name["sugar"], "original_struct": surrounding_name["pdb_id"]}
+                    merged[struct]["motifs"].extend(map(lambda x: {**motif_metadata, **x}, data["motifs"]))
+
+    with open(output, "w") as f:
+        json.dump(list(merged.values()), f, indent=4)
 
 
 
@@ -27,4 +41,4 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    main(args.source, args.output)
+    create_merged_results(args.source, args.output)
