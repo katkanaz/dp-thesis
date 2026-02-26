45bf70cbb234ece7533de75cd9de4c413cfcf079
Kateřina Nazarčuková <katka.nazarcukova@gmail.com>
2026-02-26 18:09:23 +0100

    WIP: comp_struct_handler

--- web/be/api/comp_struct_handler.go
+++ web/be/api/comp_struct_handler.go
@@ -1,1 +1,14 @@
 package api
+
+type Motif struct {}
+
+type ComputedStructure struct {
+	Pdbid string `json:"pdb_id"`
+	Afdbid string `json:"afdb_id"`
+	Title string `json:"title"`
+	Organism []string `json:"organism"`
+	Plddt float32
+	Afversion string
+	Afrevision int
+	Motifs []Motif
+}
